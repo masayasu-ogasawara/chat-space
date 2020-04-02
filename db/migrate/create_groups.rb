1f0b43c3aaad8d9eabcc7ed7457362549a577c88
masayasu-ogasawara <ogasawara.masayasu@gmail.com>
2020-04-02 15:50:56 +0900

    変更しました

--- db/migrate/create_groups.rb
+++
@@ -1,9 +0,0 @@
-class CreateGroups < ActiveRecord::Migration[5.0]
-  def change
-    create_table :groups do |t|
-      t.string :group_name, null: false
-      add_index :groups, :group_name, unique: true
-      t.timestamps
-    end
-  end
-end
