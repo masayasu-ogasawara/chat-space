5fcdb219a35cfb8438835075fecd3687c252ce08
masayasu-ogasawara <ogasawara.masayasu@gmail.com>
2020-04-09 10:29:54 +0900

    変更しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,95 +1,91 @@
-$(function(){
-  var last_message_id = $('.message:last').data("message-id");
-  console.log(last_message_id);
-  function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message">
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="message">
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
-  }
-  $('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
+function buildHTML(message){
+  if ( message.image ) {
+    var html =
+    `<div class="message">
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+        <img src=${message.image} >
+      </div>`
+    return html;
+  } else {
+    var html =
+    `<div class="message">
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+    return html;
+  };
+}
+$('#new_message').on('submit', function(e){
+e.preventDefault();
+var formData = new FormData(this);
+var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.messages').append(html);
+    $('form')[0].reset();
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    $('.form__submit').attr('disabled', false);
+  })
+  .fail(function() {
+    alert("メッセージ送信に失敗しました");
+  });
+})
+var reloadMessages = function () {
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    var last_message_id = $('.message:last').data("message-id");
+
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
+      url: "api/messages",
+      type: 'get',
       dataType: 'json',
-      processData: false,
-      contentType: false
+      data: {last_id: last_message_id}
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('form')[0].reset();
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      $('.form__submit').attr('disabled', false);
+    .done(function (messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      })
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight},);
+      }
     })
     .fail(function() {
-      alert("メッセージ送信に失敗しました");
+      alert('自動更新に失敗しました');
     });
-  })
-  var reloadMessages = function () {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){
-      var last_message_id = $('.message:last').data("message-id");
-
-      $.ajax({
-        url: "api/messages",
-        type: 'get',
-        dataType: 'json',
-        data: {last_id: last_message_id}
-      })
-      .done(function (messages) {
-        if (messages.length !== 0) {
-        var insertHTML = '';
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
-        })
-          $('.messages').append(insertHTML);
-          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-        }
-      })
-      .fail(function() {
-        alert('自動更新に失敗しました');
-      });
-    }
-  };
-  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 7000);
   }
-})
+};
+if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+}
--- config/routes.rb
+++ config/routes.rb
@@ -10,26 +10,3 @@ Rails.application.routes.draw do
     end
   end
 end
-
-
-
-# Rails.application.routes.draw do
-
-#   devise_for :users
-#   root 'groups#index'
-#   # root 'messages#index'
-#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-#   resources :users, only: [:index, :edit, :update]
-#   resources :groups, only: [:new, :create, :edit, :update] do
-#     resources :messages, only: [:index, :create]
-#     namespace :api do
-#       resources :messages, only: :index, defaults: { format: 'json' }
-#     end
-#   end
-# end
-
-# #   resources :users, only: [:index, :edit, :update]
-# #   resources :groups, only: [:new, :create, :edit, :update] do
-# #     resources :messages, only: [:index, :create]
-# #   end
-# # end
